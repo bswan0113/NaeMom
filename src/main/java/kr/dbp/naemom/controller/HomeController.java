6a5d58c1bef57ec63e331458ae718b6a3552fbe0
bswan0113 <zkoiu@naver.com>
2023-04-25 18:57:35 +0900

    마스터 병합오류 수정완료

--- src/main/java/kr/dbp/naemom/controller/HomeController.java
+++ src/main/java/kr/dbp/naemom/controller/HomeController.java
@@ -10,13 +10,10 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -25,12 +22,13 @@ import kr.dbp.naemom.vo.CourseItemVO;
 import kr.dbp.naemom.vo.CourseVO;
 import kr.dbp.naemom.vo.FileVO;
 import kr.dbp.naemom.vo.ProductVO;
-import kr.dbp.naemom.vo.MemberVO;
 
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	HomeService homeService;
 		
 	
 	@RequestMapping(value = "/")
