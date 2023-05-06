d05371449d4607606c556236c8aecdd9041c77d1
bswan0113 <zkoiu@naver.com>
2023-05-06 21:53:30 +0900

    에러페이지 구현완료

---
+++ src/main/java/kr/dbp/naemom/utils/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package kr.dbp.naemom.utils;
+
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(Exception.class)
+	public String handleException(Exception e, Model model) {
+		return "/error";
+	}
+}
