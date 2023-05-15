6564a81bdfa4c564a84d7356c0446eacac25f236
bswan0113 <116549027+bswan0113@users.noreply.github.com>
2023-05-15 20:12:55 +0900

    Delete ApiKey.java

--- src/main/java/kr/dbp/naemom/utils/ApiKey.java
+++
@@ -1,9 +0,0 @@
-package kr.dbp.naemom.utils;
-
-import lombok.Data;
-
-@Data
-public class ApiKey {
-	private final String kakaoGunwo ="9be9206bb3b872d93650ec99d8194ab4";
-	private final String BootpayKim = "aaa23";
-}
