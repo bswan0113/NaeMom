50ae89e08ab6475fedae5cdad2102d5401d40805
steve <jjojjosteve@gmail.com>
2023-05-15 18:39:19 +0900

    api.java

---
+++ src/main/java/kr/dbp/naemom/utils/ApiKey.java
@@ -0,0 +1,9 @@
+package kr.dbp.naemom.utils;
+
+import lombok.Data;
+
+@Data
+public class ApiKey {
+	private final String kakaoGunwo ="9be9206bb3b872d93650ec99d8194ab4";
+	private final String BootpayKim = "aaa23";
+}
