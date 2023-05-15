072adc9d136d1a8a5179259396362344b9c78b5c
steve <jjojjosteve@gmail.com>
2023-05-15 18:38:55 +0900

    염 로그인시 아이디/비번찾기,로그인,회원가입페이지 접근제한

--- src/main/java/kr/dbp/naemom/interceptor/MemberInterceptor.java
+++ src/main/java/kr/dbp/naemom/interceptor/MemberInterceptor.java
@@ -9,18 +9,24 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import kr.dbp.naemom.vo.MemberVO;
 
 public class MemberInterceptor extends HandlerInterceptorAdapter  {
-	
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, 
-			HttpServletResponse response, 
-			Object handler)
-			throws Exception {
-		HttpSession session = request.getSession();
-		MemberVO user = (MemberVO)session.getAttribute("user");
-		if(user == null) {
-			response.sendRedirect(request.getContextPath() + "/");
-			return false;
-		}
-		return true;
+	        HttpServletResponse response, 
+	        Object handler)
+	        throws Exception {
+	    HttpSession session = request.getSession();
+	    MemberVO user = (MemberVO) session.getAttribute("user");
+	    
+	    // 로그인이 되어있을 경우 /login, /signup 접속 제한
+	    if (user != null && (request.getRequestURI().equals(request.getContextPath() + "/login") ||
+	                         request.getRequestURI().equals(request.getContextPath() + "/signup") ||
+	                         request.getRequestURI().equals(request.getContextPath() + "/findid") ||
+	                         request.getRequestURI().equals(request.getContextPath() + "/findpw"))) {
+	        response.sendRedirect(request.getContextPath() + "/");
+	        return false;
+	    }
+	    
+	    return true;
 	}
 }
